d3aa84b895c34c6141eed9cb1827b17db6887ff5
Artem Firsov <artem.firsov@2iqresearch.com>
2019-01-27 18:18:07 +0100

    added simple menu

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -7,7 +7,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { Link } from '@material-ui/core';
+import { Link, Menu, MenuItem } from '@material-ui/core';
+import { withRouter } from 'react-router-dom'
 
 const styles = {
   root: {
@@ -22,28 +23,53 @@ const styles = {
   },
 };
 
-function ButtonAppBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-          <Link href="/" color="inherit">Home</Link>
-          </Typography>
-          <Button href="/registration" color="inherit">Registration</Button>
-          <Button href="/login" color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class MenuBar extends Component {
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+  render() {
+    const { classes } = this.props;
+    const { anchorEl } = this.state;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton onClick={this.handleClick} className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={this.handleClose}
+            >
+              <MenuItem onClick={this.handleClose} component={Link} href="/">Home</MenuItem>
+              <MenuItem onClick={this.handleClose} component={Link} href="/registration">Registration</MenuItem>
+              <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+            </Menu>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              <Link href="/" color="inherit">Home</Link>
+            </Typography>
+            <Button href="/registration" color="inherit">Registration</Button>
+            <Button href="/login" color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
-ButtonAppBar.propTypes = {
+
+MenuBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(ButtonAppBar);
+export default withStyles(styles)(MenuBar);
